6a64f881364592c9c87d784673dc7627526840c7
AKJAW <akjaworski1@gmail.com>
2020-05-10 10:46:00 +0200

    Prepared for proof of concept

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library")
+    id("com.android.application")
     id("kotlin-android")
     id("kotlin-kapt")
     id("kotlin-android-extensions")
@@ -48,4 +48,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.1.0")
     implementation("com.google.android.material:material:1.1.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta4")
+
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("androidx.test.ext:junit:1.1.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.1.1")
 }
