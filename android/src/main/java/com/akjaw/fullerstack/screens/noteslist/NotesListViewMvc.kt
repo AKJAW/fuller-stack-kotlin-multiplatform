3daa01ff06138d7b5a574a5d124cbcad512ecaac
AKJAW <akjaworski1@gmail.com>
2020-06-08 19:12:17 +0200

    A: Removed divider and styled note item with CardView

--- android/src/main/java/com/akjaw/fullerstack/screens/noteslist/NotesListViewMvc.kt
+++ android/src/main/java/com/akjaw/fullerstack/screens/noteslist/NotesListViewMvc.kt
@@ -3,13 +3,11 @@ package com.akjaw.fullerstack.screens.noteslist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.akjaw.fullerstack.android.R
 import com.akjaw.fullerstack.screens.common.ViewMvcFactory
 import com.akjaw.fullerstack.screens.common.base.BaseObservableViewMvc
-import com.akjaw.fullerstack.screens.noteslist.recyclerview.DividerItemDecorator
 import com.akjaw.fullerstack.screens.noteslist.recyclerview.NotesListAdapter
 import data.Note
 
@@ -31,10 +29,6 @@ class NotesListViewMvc(
         notesRecyclerView.adapter = notesListAdapter
         notesRecyclerView.setHasFixedSize(true)
         notesRecyclerView.layoutManager = LinearLayoutManager(context)
-        val divider = ContextCompat.getDrawable(context, R.drawable.divider)
-        if(divider != null){
-            notesRecyclerView.addItemDecoration(DividerItemDecorator(divider))
-        }
     }
 
     fun setNotes(notes: List<Note>) {
