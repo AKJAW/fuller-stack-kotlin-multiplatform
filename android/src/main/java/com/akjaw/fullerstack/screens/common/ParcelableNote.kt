b00bfcf209e59aaeddb0a261cf8854e389d845aa
AKJAW <akjaworski1@gmail.com>
2020-06-29 16:48:48 +0200

    A: Added the ability to view note details with the editor

---
+++ android/src/main/java/com/akjaw/fullerstack/screens/common/ParcelableNote.kt
@@ -0,0 +1,18 @@
+package com.akjaw.fullerstack.screens.common
+
+import android.os.Parcelable
+import data.Note
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class ParcelableNote(
+    val title: String,
+    val content: String
+) : Parcelable
+
+fun Note.toParcelable(): ParcelableNote { //TODO move to mapper
+    return ParcelableNote(
+        title = this.title,
+        content = this.content
+    )
+}
