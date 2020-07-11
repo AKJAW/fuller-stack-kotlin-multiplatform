052db8484b788a6849d8bc9f359fe30574704af6
AKJAW <akjaworski1@gmail.com>
2020-07-11 17:58:27 +0200

    A: Removed all Controller related classes

--- android/src/main/java/com/akjaw/fullerstack/composition/modules/presentationModule.kt
+++ android/src/main/java/com/akjaw/fullerstack/composition/modules/presentationModule.kt
@@ -3,7 +3,6 @@ package com.akjaw.fullerstack.composition.modules
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.akjaw.fullerstack.screens.common.ViewModelFactory
-import com.akjaw.fullerstack.screens.common.ViewMvcFactory
 import com.akjaw.fullerstack.screens.editor.NoteEditorViewModel
 import com.akjaw.fullerstack.screens.list.NotesListViewModel
 import com.akjaw.fullerstack.screens.list.recyclerview.NotesListAdapterFactory
@@ -15,7 +14,6 @@ import org.kodein.di.provider
 import org.kodein.di.singleton
 
 val presentationModule = DI.Module("presentationModule") {
-    bind() from singleton { ViewMvcFactory(instance(), instance()) }
     bind() from singleton { NotesListAdapterFactory(instance()) }
     bind<ViewModelProvider.Factory>() with singleton { ViewModelFactory(di.direct) }
     bind<ViewModel>(tag = NotesListViewModel::class.java.simpleName) with provider {
--- android/src/main/java/com/akjaw/fullerstack/screens/common/mainactivity/MainActivity.kt
+++ android/src/main/java/com/akjaw/fullerstack/screens/common/mainactivity/MainActivity.kt
@@ -2,7 +2,6 @@ package com.akjaw.fullerstack.screens.common.mainactivity
 
 import android.os.Bundle
 import com.akjaw.fullerstack.android.R
-import com.akjaw.fullerstack.screens.common.ViewMvcFactory
 import com.akjaw.fullerstack.screens.common.base.BaseActivity
 import com.akjaw.fullerstack.screens.common.navigation.keys.NotesListScreen
 import com.zhuinden.simplestack.History
@@ -10,24 +9,20 @@ import com.zhuinden.simplestack.SimpleStateChanger
 import com.zhuinden.simplestack.StateChange
 import com.zhuinden.simplestack.navigator.Navigator
 import com.zhuinden.simplestackextensions.fragments.DefaultFragmentStateChanger
-import org.kodein.di.instance
 
 class MainActivity : BaseActivity(), SimpleStateChanger.NavigationHandler {
 
-    private val viewMvcFactory: ViewMvcFactory by instance<ViewMvcFactory>()
-    private lateinit var viewMvc: MainViewMvc
     private lateinit var fragmentStateChanger: DefaultFragmentStateChanger
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        viewMvc = viewMvcFactory.getMainViewMvc(null)
-        setContentView(viewMvc.rootView)
+        setContentView(R.layout.activity_main)
 
         fragmentStateChanger = DefaultFragmentStateChanger(supportFragmentManager, R.id.fragment_placeholder)
         Navigator.configure()
             .setStateChanger(SimpleStateChanger(this))
-            .install(this, viewMvc.rootView.findViewById(R.id.root), History.of(NotesListScreen()))
+            .install(this, findViewById(R.id.root), History.of(NotesListScreen()))
     }
 
     override fun onBackPressed() {
