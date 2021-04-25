aa5ab1c4f69c69b020c638a2df99dc364da92038
AKJAW <akjaworski1@gmail.com>
2021-04-25 09:30:31 +0200

    K: Add table creation to PostgreSQL database

--- ktor/src/main/kotlin/server/storage/PostgreSqlDatabase.kt
+++ ktor/src/main/kotlin/server/storage/PostgreSqlDatabase.kt
@@ -3,10 +3,17 @@ package server.storage
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import server.storage.model.NotesTable
 
 @Suppress("MagicNumber")
 class PostgreSqlDatabase(databaseUrl: String) : ExposedDatabase {
-    private val database: Database = Database.connect(hikari(databaseUrl))
+    private val database: Database = Database.connect(hikari(databaseUrl)).apply {
+        transaction {
+            SchemaUtils.createMissingTablesAndColumns(NotesTable)
+        }
+    }
 
     private fun hikari(databaseUrl: String): HikariDataSource {
         val config = HikariConfig()
